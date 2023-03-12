8e3ac52a0079a14310e787ea98b5edc21eae01aa
shubhamserva <shubhamserva1990@gmail.com>
2023-03-12 14:37:55 +0530

    backend url

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,6 +1,6 @@
 let BACKEND_SERVER = null;
 
-BACKEND_SERVER = "http://68.183.95.182:5000/api/";
+BACKEND_SERVER = "https://68.183.95.182:5000/api/";
 //BACKEND_SERVER = "http://localhost:5000/api/"
 
 
